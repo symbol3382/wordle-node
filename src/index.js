2d87b06e150368956f4ab463d0b9c15189d447c4
Gourav Verma <gourav.verma@robomq.io>
2023-12-27 15:36:27 +0530

    Added https

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const guessRoutes = require('./routes/guess');
 const cronRoutes = require('./routes/cron');
 const mysql = require("./config/db");
 const cors = require('cors');
+const https = require('https');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(guessRoutes);
@@ -16,9 +17,18 @@ mysql.init();
 
 const hostname = process.env.NODE_HOSTNAME;
 if (hostname) {
-    app.listen(process.env.APP_PORT, hostname, () => {
-        console.log('APP Started on ', process.env.APP_PORT, hostname);
-    })
+    if (process.env.SSL_PEM) {
+        console.log('Running app with SSL')
+        var options = {
+            key: fs.readFileSync(process.env.SSL_PEM),
+            cert: fs.readFileSync(process.env.SSL_CERT)
+        };
+        https.createServer(options, app).listen(process.env.APP_PORT);
+    } else {
+        app.listen(process.env.APP_PORT, hostname, () => {
+            console.log('APP Started on ', process.env.APP_PORT, hostname);
+        })
+    }
 } else {
     app.listen(process.env.APP_PORT, () => {
         console.log('APP Started on ', process.env.APP_PORT);
